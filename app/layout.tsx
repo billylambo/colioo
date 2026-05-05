5774d1b3f124372631703fdfdc66426f10faf52c
JUNIOR <mrdroit01@gmail.com>
2026-05-05 14:13:26 +0000

    perf: SSR complet homepage + page produit, pixels différés, compression WebP

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       className={`${poppins.variable} ${inter.variable} h-full antialiased`}
     >
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -41,8 +41,11 @@ export default function RootLayout({
         <link rel="icon" href="/icon-32.png" sizes="32x32" />
         <link rel="icon" href="/icon-192.png" sizes="192x192" />
 
-        {/* ── Google Analytics 4 ── */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VZHWEYHVEM" />
+        {/* ── GA4 — non bloquant ── */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VZHWEYHVEM"
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -57,56 +60,46 @@ export default function RootLayout({
           }}
         />
 
-        {/* ── Facebook Pixel ── */}
+        {/* ── Facebook Pixel + TikTok Pixel — chargés après la page ── */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1772392297075686');
-              fbq('track', 'PageView');
-            `
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1772392297075686&ev=PageView&noscript=1"
-          />
-        </noscript>
+              window.addEventListener('load', function() {
+                // Facebook Pixel
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1772392297075686');
+                fbq('track', 'PageView');
 
-        {/* ── TikTok Pixel ── */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function (w, d, t) {
-                w.TiktokAnalyticsObject=t;
-                var ttq=w[t]=w[t]||[];
-                ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],
-                ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-                for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-                ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},
-                ttq.load=function(e,n){
-                  var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
-                  ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,
-                  ttq._t=ttq._t||{},ttq._t[e]=+new Date,
-                  ttq._o=ttq._o||{},ttq._o[e]=n||{};
-                  n=document.createElement("script");
-                  n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;
-                  e=document.getElementsByTagName("script")[0];
-                  e.parentNode.insertBefore(n,e)
-                };
-                ttq.load('D7RKJJJC77U9UIR3K49G');
-                ttq.page();
-              }(window, document, 'ttq');
+                // TikTok Pixel
+                !function (w, d, t) {
+                  w.TiktokAnalyticsObject=t;
+                  var ttq=w[t]=w[t]||[];
+                  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+                  ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
+                  for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+                  ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
+                  ttq.load=function(e,n){
+                    var r="https://analytics.tiktok.com/i18n/pixel/events.js";
+                    ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,
+                    ttq._t=ttq._t||{},ttq._t[e]=+new Date,
+                    ttq._o=ttq._o||{},ttq._o[e]=n||{};
+                    n=document.createElement("script");
+                    n.type="text/javascript",n.async=!0,
+                    n.src=r+"?sdkid="+e+"&lib="+t;
+                    e=document.getElementsByTagName("script")[0];
+                    e.parentNode.insertBefore(n,e)
+                  };
+                  ttq.load('D7RKJJJC77U9UIR3K49G');
+                  ttq.page();
+                }(window, document, 'ttq');
+              });
             `
           }}
         />
